06e3f194e91be7320b5a5bcd49ca5dc622695bfc
anasiavara-jpg <ana.sia.vara@gmail.com>
2025-09-25 18:05:29 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-pip install --upgrade --force-reinstall python-telegram-bot==20.7
-pip install aiohttp python-dotenv nest_asyncio
+pip uninstall -y python-telegram-bot
+pip install --no-cache-dir --upgrade --force-reinstall python-telegram-bot==20.7
+pip install --no-cache-dir aiohttp python-dotenv nest_asyncio
 python main.py
