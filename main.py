fa419b0f74a91e2eb5f87156991f4e00ae82cc75
anasiavara-jpg <ana.sia.vara@gmail.com>
2025-09-25 22:20:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import json
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from dotenv import load_dotenv
 
 # ---------------- CONFIG ----------------
@@ -18,7 +19,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 logging.basicConfig(level=logging.INFO)
