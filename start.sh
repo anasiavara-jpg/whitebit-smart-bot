a44bdc39a395c870d6c1948d0c795e6f16a0fd9f
anasiavara-jpg <ana.sia.vara@gmail.com>
2025-09-30 15:05:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-set -e  # зупинятись при помилках
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-exec python3 main.py
+pip install -r requirements.txt
+python3 main.py
