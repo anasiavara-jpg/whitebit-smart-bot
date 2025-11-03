22e83afb55a8ea6301dd416d660e5c8cff033bfa
anasiavara-jpg <ana.sia.vara@gmail.com>
2025-11-03 19:43:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1346,8 +1346,7 @@ async def ensure_single_instance():
 
 # Виклик перед запуском polling:
 await ensure_single_instance()
-await dp.start_polling(bot)
-    await dp.start_polling(bot, skip_updates=True)
+await dp.start_polling(bot, skip_updates=True)
 
 if __name__ == "__main__":
     try:
